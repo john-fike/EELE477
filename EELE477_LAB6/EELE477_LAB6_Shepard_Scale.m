4790ad3d20b580b09036cfcd79ab2efdb7ce634d
john-fike <johnfike223@gmail.com>
2023-03-08 19:57:09 -0700

    play C chord a bunch fuck wait i wasn't supposed to do that FUCK

--- EELE477_LAB6/EELE477_LAB6_Shepard_Scale.m
+++ EELE477_LAB6/EELE477_LAB6_Shepard_Scale.m
@@ -1,20 +1,25 @@
 clc;
 clear;
 
-cScale = key2note(1, 42, 2);
+noteDur = .5
+cScale = [key2note(1, 42, noteDur) zeros(1,500)];
 cScale = zeros(1,length(cScale))
 note = 49;
 bottomNote = note - 12*4;
 topNote = note + 12*4;
 
+
+%     cScale = cScale + [key2note(1, 49, 2) zeros(1,500)];
+%     cScale = cScale + key2note(1, 55, 2);
+
+
 for i = 1:12:97
-    cScale = cScale + key2note(1, i, 2);
-    cScale = [cScale zeros(1,500)];
+    cScale = cScale + [key2note(1, i, noteDur) zeros(1,500)];
 end
 
-% for i = 1:8
-%     cScale = [cScale cScale];
-% end
+for i = 1:8
+    cScale = [cScale cScale];
+end
 
 
 soundsc(cScale);
